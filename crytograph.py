c99477ce023ce22f250fb124563da0f4f1d99a30
Muhammad Luqni Baehaqi <mluqnibaehaqi@gmail.com>
2018-09-28 15:50:35 +0700

    second

--- crytograph.py
+++ crytograph.py
@@ -1,7 +1,5 @@
 from Tkinter import *
-import tkMessageBox as mb
-import ttk
-import pyperclip
+
 
  
 # MUHAMMAD LUQNI BAEHAQI
@@ -14,15 +12,17 @@ class crypto:
   
         self.induk.resizable(True, True)
          
-        self.aturKomponen()
+        self.sandiBalik()
          
-        self.entUser.focus_set()
+        # self.entUser.focus_set()
 
 	self.membuatMenu()
 
 	self.sandiCaesar()
+
+	# self.decryptCaesar()
          
-    def aturKomponen(self):
+    def sandiBalik(self):
         # atur frame utama
         frameUtama = Frame(self.induk, bd=10)
         frameUtama.pack(fill=BOTH, expand=YES)
@@ -34,8 +34,8 @@ class crypto:
         # atur input username
         Label(frData, text=':: Sandi Balik ::').grid(row=0, column=1, sticky=W)
         Label(frData, text='Masukkan Teks : ').grid(row=2, column=0, sticky=W)
-        self.entUser = Entry(frData)
-        self.entUser.grid(row=2, column=1)
+        self.balik = Entry(frData)
+        self.balik.grid(row=2, column=1)
          
         # menampilkan hasil
         Label(frData, text='Hasil : ').grid(row=3, column=0)
@@ -50,7 +50,9 @@ class crypto:
         # atur tombol login
         self.btnLogin = Button(frTombol, text='Proses', command=self.prosesBalik)
         self.btnLogin.pack(side=LEFT, fill=BOTH, expand=YES)
+    
     def sandiCaesar(self):
+    	var = IntVar()
         # atur frame utama
         frameUtama = Frame(self.induk, bd=10)
         frameUtama.pack(fill=BOTH, expand=YES)
@@ -61,19 +63,27 @@ class crypto:
          
         # atur input username
         Label(frData2, text=':: Sandi Caesar ::').grid(row=0, column=1, sticky=W)
-        Label(frData2, text='Masukan Teks : ').grid(row=1, column=0, sticky=W)
+
+        # Radiobutton(frData2, text="encrypt", variable=var, value=1).grid(row=1, column=1, sticky=W)
+        # self.var = Entry(frData2)
+        # Radiobutton(frData2, text="decrypt", variable=var, value=2).grid(row=2, column=1, sticky=W)
+        Label(frData2, text='encrypt/decrypt : ').grid(row=2, column=0, sticky=W)
+        self.var = Entry(frData2)
+        self.var.grid(row=2, column=1)
+        
+        Label(frData2, text='Masukan Teks : ').grid(row=3, column=0, sticky=W)
         self.Caesar = Entry(frData2)
-        self.Caesar.grid(row=1, column=1)
+        self.Caesar.grid(row=3, column=1)
         # memasukan kunci
-        Label(frData2, text='Masukan Kunci : ').grid(row=2, column=0, sticky=W)
+        Label(frData2, text='Masukan Kunci : ').grid(row=4, column=0, sticky=W)
         self.Caesar2 = Entry(frData2)
-        self.Caesar2.grid(row=2, column=1)
+        self.Caesar2.grid(row=4, column=1)
          
         # menampilkan hasil
-        Label(frData2, text='Hasil :').grid(row=3, column=0)
+        Label(frData2, text='Hasil :').grid(row=5, column=0)
 	
         self.hasil2 = Entry(frData2)
-        self.hasil2.grid(row=3, column=1)
+        self.hasil2.grid(row=5, column=1)
          
         # atur frame tombol
         frTombol2 = Frame(frameUtama, bd=5)
@@ -85,7 +95,7 @@ class crypto:
          
     def prosesBalik(self, event=None):
         # ambil data input dari pengguna
-        pesan = self.entUser.get()
+        pesan = self.balik.get()
 	ubah = ''
 	i = len(pesan)-1
 	while i >= 0:
@@ -95,13 +105,17 @@ class crypto:
     def caesar(self):
 	pesan2 = self.Caesar.get()
 	kunci = eval(self.Caesar2.get())
+	mode = str(self.var.get())
 	huruf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 	ubah2 = ''
 	pesan2 = pesan2.upper()
 	for symbol in pesan2:
 		if symbol in huruf:
 			nomor = huruf.find(symbol)
-			nomor = nomor+kunci
+			if mode == 'encrypt':
+				nomor = nomor+kunci
+			elif mode == 'decrypt':
+				nomor = nomor - kunci
 			if nomor >= len(huruf):
 				nomor = nomor - len(huruf)
 			elif nomor < 0:
@@ -110,6 +124,30 @@ class crypto:
 		else:
 			ubah2 = ubah2 + symbol
 	self.hasil2.insert(0, ubah2)
+
+	# def DecryptCaesar(self):
+	# 	pesan3 = self.DecryptCaesar.get()
+	
+	# 	kunci2 = eval(self.DecryptCaesar2.get())
+	# 	huruf2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	# 	ubah3 = ''
+	# 	pesan3 = pesan3.upper()
+	# 	for symbol in pesan3:
+	# 		if symbol in huruf2:
+	# 			nomor2 = huruf2.find(symbol)
+	# 			nomor2 = nomor2-kunci2
+	# 			if nomor2 >= len(huruf2):
+	# 				nomor2 = nomor2 - len(huruf2)
+	# 			elif nomor2 < 0:
+	# 				nomor2 = nomor2 + len(huruf2)
+	# 			ubah3 = ubah3 + huruf2[nomor2]
+	# 		else:
+	# 			ubah3 = ubah3 + symbol
+	# 	self.hasil3.insert(0, ubah3)
+
+
+
+
     def membuatMenu(self):
 	menubar = Menu(self.induk)
 	self.induk.config(menu=menubar)
@@ -119,9 +157,12 @@ class crypto:
 	self.induk.config(menu=menubar)
 
 	fileMenu = Menu(menubar)
-	fileMenu.add_command(label="Nama Kelompok",command='')
+	fileMenu.add_command(label="Alfa paizun",command='')
+	fileMenu.add_command(label="Hasna M",command='')
+	fileMenu.add_command(label="Luqni B",command='')
+	fileMenu.add_command(label="Tika A",command='')
 	fileMenu.add_command(label="Exit",command=self.perintahKeluar)
-	menubar.add_cascade(label="File",menu=fileMenu)
+	menubar.add_cascade(label="Nama Kelompok",menu=fileMenu)
     def perintahKeluar(self):
 	self.induk.destroy()
          
